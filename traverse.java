08d733b6b510b6ab8a345ae9141eb30ef5afb6e2
Pranay Ashok  Gajbe <100502178+PranayGajbe@users.noreply.github.com>
2023-06-16 09:43:18 +0530

    Create traverse.java

---
+++ traverse.java
@@ -0,0 +1,19 @@
+# firstproject
+import java.io.*;
+public class traverse
+{
+    public static void main(String[] args) throws IOException
+    {
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        int i, x;
+
+        for ( i = 0; i < arr.length; i++)
+        {
+            x = arr[i];
+
+            System.out.print(x + " ");
+        }
+    }
+}
