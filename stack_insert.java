82af6fc74002bc9a67173dc76fb54bfefa8e445f
Pranay Ashok  Gajbe <100502178+PranayGajbe@users.noreply.github.com>
2023-06-16 09:53:00 +0530

    Create stack_insert.java

---
+++ stack_insert.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+class insert
+
+{
+	public static void main(String args[])
+	{
+		Stack<String> s1 = new Stack<>();
+
+		s1.push("10");
+		s1.push("20");
+		s1.push("30");
+		s1.push("40");
+
+		System.out.println();
+		System.out.println(s1);
+	}
+}
