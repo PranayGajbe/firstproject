d66146383f353bdc0da013acb392a89c60b01e12
Pranay Ashok  Gajbe <100502178+PranayGajbe@users.noreply.github.com>
2023-06-16 09:45:29 +0530

    Create linked_list.java

---
+++ linked_list.java
@@ -0,0 +1,27 @@
+# firstproject
+import java.util.*;
+
+class insert
+{
+	public static void main(String args[])
+	{
+		LinkedList<String> l1 = new LinkedList();
+
+		l1.add("one");
+		l1.add("two");
+		l1.add("three");
+
+		for(int i = 0; i < l1.size(); i++)
+		{
+			String x = l1.get(i);
+			System.out.println( x + " ");
+		}
+
+		System.out.println("__________________________");
+System.out.println("hello world");
+
+
+
+	}
+}
+		
